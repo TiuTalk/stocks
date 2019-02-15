592f4050a7ac5bc2c945f9955f316941a3d73723
Thiago Belem <contato@thiagobelem.net>
2019-02-15 19:55:09 -0300

    Add specs for exception rescue blocks

--- spec/services/alpha_vantage/client_spec.rb
+++ spec/services/alpha_vantage/client_spec.rb
@@ -9,6 +9,19 @@ RSpec.describe AlphaVantage::Client, type: :service do
         output = client.timeseries('ITSA4.SAO', outputsize: 'compact')
         expect(output.first).to include(open: 13.29, high: 13.34, low: 13.06, volume: 16_697_600)
       end
+
+      context 'TooManyRequestsException' do
+        it 'expire the key and raise error' do
+          error_data = { 'Note' => 'Our standard API call frequency is' }
+          error = Alphavantage::Error.new(message: 'Invalid API call', data: error_data)
+          expect_any_instance_of(Alphavantage::Stock).to receive(:timeseries).and_raise(error)
+          expect_any_instance_of(AlphaVantage::ApiKey).to receive(:expire)
+
+          expect do
+            client.timeseries('TEST')
+          end.to raise_error(Alphavantage::Error)
+        end
+      end
     end
 
     context 'with invalid symbol' do
--- spec/workers/quotes_importer_worker_spec.rb
+++ spec/workers/quotes_importer_worker_spec.rb
@@ -4,13 +4,37 @@ RSpec.describe QuotesImporterWorker, type: :worker do
   let(:stock) { double(Stock, enabled?: true) }
   let(:importer) { double(QuotesImporter, call_async: true) }
 
+  before do
+    expect(Stock).to receive(:find).with('id').and_return(stock)
+    expect(QuotesImporter).to receive(:new).with(stock).and_return(importer)
+  end
+
   context 'with valid stock_id' do
     it 'call QuotesImporter service' do
-      expect(Stock).to receive(:find).with('id').and_return(stock)
-      expect(QuotesImporter).to receive(:new).with(stock).and_return(importer)
       expect(importer).to receive(:call_async)
-
       described_class.new.perform('id')
+    end
+  end
+
+  context 'AlphaVantage::ApiKey::MissingAvailableKey' do
+    it 'reschedule the QuotesImporterWorker' do
+      expect(importer).to receive(:call_async).and_raise(AlphaVantage::ApiKey::MissingAvailableKey)
+
+      expect do
+        described_class.new.perform('id')
+      end.to change(described_class.jobs, :count).by(1)
+    end
+  end
+
+  context 'AlphaVantage::Client::TooManyRequestsException' do
+    it 'reschedule the QuotesImporterWorker' do
+      error_data = { 'Note' => 'Our standard API call frequency is' }
+      error = Alphavantage::Error.new(message: 'Invalid API call', data: error_data)
+      expect(importer).to receive(:call_async).and_raise(error)
+
+      expect do
+        described_class.new.perform('id')
+      end.to change(described_class.jobs, :count).by(1)
     end
   end
 end
