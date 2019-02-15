3b4c0c507a86447d016c76d50d57770e2b86f0a5
Thiago Belem <contato@thiagobelem.net>
2019-02-15 15:05:34 -0300

    Prevent Quote date to be today or in the future

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -5,4 +5,11 @@ class Quote < ApplicationRecord
   # Validations
   validates :stock, :date, presence: true
   validates :date, uniqueness: { scope: :stock_id }
+  validate :date_is_in_the_past
+
+  private
+
+  def date_is_in_the_past
+    errors.add(:date, 'should be in the past') if date.present? && (date.today? || date.future?)
+  end
 end
--- spec/models/quote_spec.rb
+++ spec/models/quote_spec.rb
@@ -13,6 +13,11 @@ RSpec.describe Quote, type: :model do
     context 'date' do
       subject { build(:quote) }
       it { is_expected.to validate_uniqueness_of(:date).scoped_to(:stock_id) }
+
+      it 'only accept dates in the past' do
+        expect(subject).to allow_values(1.day.ago, 2.days.ago, 10.years.ago).for(:date)
+        expect(subject).to_not allow_values(1.minute.ago, Time.zone.today, 1.day.from_now).for(:date)
+      end
     end
   end
 end
