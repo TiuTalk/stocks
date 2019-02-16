81317a59ff1eb910ac261984586f6f3d7e4eb05c
Thiago Belem <contato@thiagobelem.net>
2019-02-16 14:14:15 -0300

    Remove log message

--- app/workers/quotes_importer_worker.rb
+++ app/workers/quotes_importer_worker.rb
@@ -8,16 +8,8 @@ class QuotesImporterWorker
 
     QuotesImporter.new(stock).call_async
   rescue AlphaVantage::ApiKey::MissingAvailableKey
-    log('Missing available API key.. rescheduling')
     QuotesImporterWorker.perform_in(rand(1..5).minutes, stock_id)
   rescue AlphaVantage::Client::TooManyRequestsException
-    log('Too many requests.. rescheduling')
     QuotesImporterWorker.perform_in(rand(10..30).minutes, stock_id)
-  end
-
-  private
-
-  def log(message)
-    logger.info(message) unless Rails.env.test?
   end
 end
