6daa4bd03af25eb65ccfa6c92da77d3a6a71dedc
Thiago Belem <contato@thiagobelem.net>
2019-02-16 20:32:58 -0300

    Delete holding record if quantity gets to zero

--- app/models/holding.rb
+++ app/models/holding.rb
@@ -2,4 +2,13 @@ class Holding < ApplicationRecord
   # Associations
   belongs_to :wallet, inverse_of: :holdings
   belongs_to :stock, inverse_of: :holdings
+
+  # Callbacks
+  after_save :check_quantity, if: :saved_change_to_quantity?
+
+  private
+
+  def check_quantity
+    destroy if quantity.zero?
+  end
 end
--- spec/models/holding_spec.rb
+++ spec/models/holding_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe Holding, type: :model do
     it { is_expected.to belong_to(:wallet).inverse_of(:holdings) }
     it { is_expected.to belong_to(:stock).inverse_of(:holdings) }
   end
+
+  describe 'callbacks' do
+    context 'after_save' do
+      it 'destroy holding if quantity is zero' do
+        holding = create(:holding)
+
+        expect do
+          holding.update(quantity: 0)
+        end.to change(Holding, :count).by(-1)
+      end
+    end
+  end
 end
