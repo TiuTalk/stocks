5d538f01753219c37938ebb5ec995599a670b6ed
Thiago Belem <contato@thiagobelem.net>
2019-02-15 22:30:25 -0300

    Fix Sector associations

--- app/models/sector.rb
+++ app/models/sector.rb
@@ -2,8 +2,8 @@ class Sector < ApplicationRecord
   # Associations
   belongs_to :stock_exchange, inverse_of: :sectors
   has_many :stocks, inverse_of: :sector, dependent: :destroy
-  has_many :fiis, class_name: 'FII', inverse_of: :stock_exchange, dependent: :destroy
-  has_many :etfs, class_name: 'ETF', inverse_of: :stock_exchange, dependent: :destroy
+  has_many :fiis, class_name: 'FII', inverse_of: :sector, dependent: :destroy
+  has_many :etfs, class_name: 'ETF', inverse_of: :sector, dependent: :destroy
 
   # Validations
   validates :name, presence: true
