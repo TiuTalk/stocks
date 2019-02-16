248199ec79b2c6e1e07b6bda5468284fbe7d7b54
Thiago Belem <contato@thiagobelem.net>
2019-02-16 20:33:17 -0300

    Add password to User factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
   factory :user do
     name { 'John Doe' }
     email { "user+#{SecureRandom.hex(5).downcase}@example.com" }
+    password { 'password!' }
+    password_confirmation { password }
   end
 end
