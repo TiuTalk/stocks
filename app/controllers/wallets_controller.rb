df96b43ec12b47c2a70a829ba51fce17a0704a1e
Thiago Belem <contato@thiagobelem.net>
2019-03-03 20:36:51 -0300

    Remove WalletReport

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,12 +1,5 @@
 class WalletsController < ApplicationController
   def show
     @wallet = Wallet.find(params[:id])
-    @report = WalletReport.new(@wallet, wallet_report_params.to_unsafe_hash)
-  end
-
-  private
-
-  def wallet_report_params
-    params.permit(:interval)
   end
 end
